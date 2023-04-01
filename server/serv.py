df27008186f7fc49d0f0803b06a403ba51a08898
Kelton01 <4keltonbenson@gmail.com>
2023-03-31 18:27:11 -0700

    Add files via upload

--- server/serv.py
+++ server/serv.py
@@ -1,11 +1,26 @@
 from socket import *
+import os
 import sys
 
+#sender = connect, receiver = bind
 
 serverPort = sys.argv[1]
 
-print(serverPort)
-
-serverSocket = socket(AF_INET, SOCK_STREAM)
+serverSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 serverSocket.bind(('',serverPort))
+
+print("The Server is ready to receive")
+
+file = open("image.png", "rb")
+file_size = os.path.getsize("image.png")
+
+serverSocket.send("received_image.png".encode())
+serverSocket.send(str(file_size).encode())
+
+data = file.read()
+serverSocket.sendall(data)
+serverSocket.send(b"<END>")
+
+file.close()
+serverSocket.close()
