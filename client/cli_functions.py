22e5dfb1570a666da64a9e61fae4c1eab3136a06
Kelton01 <4keltonbenson@gmail.com>
2023-04-21 16:09:26 -0700

    Add files via upload

--- client/cli_functions.py
+++ client/cli_functions.py
@@ -14,15 +14,14 @@ def download(serverName, serverPort, file_name):
     connectionSocket, addr = dataSocket.accept()
 
     file_name = connectionSocket.recv(1024).decode()
-    print("FILE NAME: " + file_name)
     file_size = connectionSocket.recv(1024).decode()
 
     file = open(file_name, "wb")
 
     file_bytes = b""
 
+    print("Downloading: " + file_name + " from server.")
     done = False
-    print("Sending File...")
     while not done:
         data = connectionSocket.recv(1024)
         if file_bytes[-3:] == b"EOF":
@@ -34,16 +33,18 @@ def download(serverName, serverPort, file_name):
     file.write(file_bytes[0:-3])
     file.close()
     dataSocket.close()
+    print("Success: Downloaded " + file_size + " bytes        ")
 
 def upload(serverName, serverPort, file_name):
     time.sleep(1)
     dataSocket = socket(AF_INET, SOCK_STREAM)
     dataSocket.connect(('localhost', 5001))
 
-    print("FILE NAME:" + file_name)
 
     file = open(file_name, "rb")
     file_size = os.path.getsize(file_name)
+
+    print("Uploading: " + file_name + " to server.")
 
     dataSocket.send(file_name.encode())
     dataSocket.send(str(file_size).encode())
@@ -53,6 +54,7 @@ def upload(serverName, serverPort, file_name):
     dataSocket.send(b"EOF")
     file.close()
     dataSocket.close()
+    print("File Upload Successful")
 
     
 
